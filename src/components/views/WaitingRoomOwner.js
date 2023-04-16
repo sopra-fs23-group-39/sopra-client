d57b4e13dc8165812c551bec4fb9970db4256002
Yannick <yannick.salzmann@hotmail.ch>
2023-04-16 12:22:14 +0200

    fixed deployment problem

--- src/components/views/WaitingRoomOwner.js
+++ src/components/views/WaitingRoomOwner.js
@@ -7,7 +7,7 @@ import "styles/views/WaitingRoom.scss";
 
 const WaitingRoomOwner = () => {
     const history = useHistory();
-    const [users, setUsers] = useState(null);
+    const [setUsers] = useState(null);
 
 
     useEffect(() => {
@@ -23,7 +23,7 @@ const WaitingRoomOwner = () => {
         }
 
         fetchData();
-    }, []);
+    }, [setUsers]);
 
     return (
         <BaseContainer>
