6b170ee2e72574c45f467b3f521bfad5cac1e35a
NattiShakira <82164378+NattiShakira@users.noreply.github.com>
2023-04-25 11:26:27 +0200

    Fix bugs in Standings.js, issue #53

--- src/components/views/Standings.js
+++ src/components/views/Standings.js
@@ -1,101 +1,122 @@
 import BaseContainer from "components/ui/BaseContainer";
-import {useHistory} from 'react-router-dom';
-import { useEffect, useState } from "react";
+import {useHistory, useParams} from 'react-router-dom';
+import { useEffect, useState, useRef } from "react";
 import {api, handleError} from 'helpers/api';
 import 'styles/views/Standings.scss'
 
-const Standings = async () => {
-
-    const lobbyId = localStorage.getItem("lobbyId");
-    const gameId = lobbyId;
+function Standings() {
+    const {gameId} = useParams();
     const history = useHistory();
     const [currentRanking, setCurrentRanking] = useState([]);
     const [totalRanking, setTotalRanking] = useState([]);
 
-    //const correctAnswer = await api.get(`/game/questions/${lobbyId}`);
-    const correctAnswer = "Placeholder";
-    
-    const fetchCurrentRanking = async () => {
-        try {
-          const response = await api.get(`/lobbies/${lobbyId}/currentRanking`);
-          setCurrentRanking(response.data.currentRanking);
-        } catch (error) {
-          alert(`Something went wrong while fetching the current ranking: \n${handleError(error)}`);
-        }
-      }
-      
-
-    const fetchTotalRanking = async () => {
-        try {
-          const response = await api.get(`/lobbies/${lobbyId}/totalRanking`);
-          setTotalRanking(response.data.totalRanking);
-        } catch (error) {
-          alert(`Something went wrong while fetching the total ranking: \n${handleError(error)}`);
-        }
-      }
+    const timeoutRef = useRef(null);
+    const [questionAmount, setQuestionAmount] = useState();
+    const [currentRound, setCurrentRound] = useState();
 
     useEffect(() => {
+        async function fetchCurrentRanking() {
+            try {
+                const response = await api.get(`/game/${gameId}/currentRanking`);
+                setCurrentRanking(response.data);
+                console.log("Fetched currentRanking:");
+                console.log(response.data);
+            } catch (error) {
+                alert(`Something went wrong while fetching the current ranking: \n${handleError(error)}`);
+            }
+        }
+
         fetchCurrentRanking();
-        fetchTotalRanking();
+
     }, []);
 
-    const response = await api.get(`/game/${gameId}/settings`);
-    const { questionAmount, currentRound } = response.data;
-
-    try {
-        if (questionAmount !== currentRound) {
-          setTimeout(() => {
-            history.push(`/game/${gameId}/question`);
-          }, 6000); // Wait for 6 seconds before routing to game question page
-        } else {
-          setTimeout(() => {
-            history.push('/main');
-          }, 6000); // Wait for 6 seconds before routing to main page
+    useEffect(() => {
+        async function fetchTotalRanking() {
+            try {
+                const response = await api.get(`/game/${gameId}/totalRanking`);
+                setTotalRanking(response.data);
+                console.log("Fetched totalRanking:");
+                console.log(response.data);
+            } catch (error) {
+                alert(`Something went wrong while fetching the total ranking: \n${handleError(error)}`);
+            }
         }
-      } catch (error) {
-        alert(`Something went wrong during game navigation: \n${handleError(error)}`);
-      }
+
+        fetchTotalRanking();
+
+    }, []);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+            const response = await api.get(`/game/${gameId}/settings`);
+            setQuestionAmount(response.data.questionAmount);
+            setCurrentRound(response.data.currentRound);
+            console.log('request to:', response.request.responseURL);
+            console.log('status code:', response.status);
+            console.log('status text:', response.statusText);
+            console.log('requested data:', response.data);
+            console.log("Game settings:")
+            console.log(response);
+
+        } catch (error) {
+            console.error(`Something went wrong while fetching the game settings: \n${handleError(error)}`);
+            console.error("Details:", error);
+            alert("Something went wrong while fetching the game settings! See the console for details.");
+        }
+    }
+
+    fetchData();
+}, []);
+
+    useEffect(() => {
+        try {
+            //let path = '/main';
+            if (questionAmount !== currentRound) {
+                timeoutRef.current = setTimeout(() => {
+                    history.push(`/game/${gameId}/question`);
+                }, 60000);
+            } else {
+                timeoutRef.current = setTimeout(() => {
+                    history.push("/main");
+                }, 60000);
+            }
+        } catch (error) {
+            alert(`Something went wrong during game navigation: \n${handleError(error)}`);
+        }
+
+        // return () => {
+        //     clearTimeout(timeoutRef.current);
+        // };
+    }, [history, gameId]);
 
     return (
         <BaseContainer>
             <div className="correct Answer">
-                <h1>Solution:</h1>
-                <p>The correct Answer is: {correctAnswer} </p>
+                <h1>Standings:</h1>
              </div>
              <div className="This rounds ranking">
-                <h1>This rounds ranking:</h1>
-                <ul>
-                    {currentRanking.map((player, index) => (
-                        <ul>
-                        .sort((a, b) => b.currentPoints - a.currentPoints)
-                        .map((player, index) => (
-                          <li key={index}>
-                            {player.username} - {player.currentPoints}
-                          </li>
-                        ))}
-                    </ul>
+                <h1>This round's ranking:</h1>
+                <ol>
+                    {currentRanking.map((player) => (
+                      <li key={player.id}>
+                        {player.username} - {player.currentPoints}
+                      </li>
                     ))}
-                </ul>
+                </ol>
             </div>
             <div className="Total Ranking">
-                <h1>Total Ranking:</h1>
-                <ul>
-                    {totalRanking.map((player, index) => (
-                        <ul>
-                        {totalRanking
-                        .sort((a, b) => b.totalPoints - a.totalPoints)
-                        .map((player, index) => (
-                          <li key={index}>
-                            {player.username} - {player.totalPoints}
-                          </li>
-                        ))}
-                    </ul>
+                <h1>Total ranking:</h1>
+                <ol>
+                    {totalRanking.map((player) => (
+                      <li key={player.id}>
+                          {player.username} - {player.totalPoints}
+                      </li>
                     ))}
-                </ul>
+                </ol>
             </div>
         </BaseContainer>
-            
     );
-};
+}
 
 export default Standings;
