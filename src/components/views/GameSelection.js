170f43ca96a88ad34e7b2db4a4c9a497e3890edc
florencehuegi <92146248+florencehuegi@users.noreply.github.com>
2023-05-16 17:57:16 +0200

    updated GameFormat & GameSelection

--- src/components/views/GameSelection.js
+++ src/components/views/GameSelection.js
@@ -34,64 +34,8 @@ const GameSelection = () => {
         but5: color
     });
 
-    const handleCustom = () => {
-        history.push('/game_custom');
-    };
-    
-    const handleRapid = async () => {
-        try {
-          setGameFormat('Rapid');
-          const hostId = localStorage.getItem('id');
-          const requestBody = JSON.stringify({
-            hostId,
-            gameMode,
-            questionAmount,
-            timer: timerValue,
-            gameFormat,
-          });
-          const response = await api.post('/game', requestBody);
-          const gameId = response.data.gameId;
-          history.push(`/rapid_selection/${gameId}`);
-        } catch (error) {
-          console.error(`Something went wrong while creating the game: \n${handleError(error)}`);
-          console.error('Details:', error);
-          alert('Something went wrong while creating the game! See the console for details.');
-        }
-    };
-    
-    const handleBlitz = async () => {
-        try {
-          setGameFormat('Blitz');
-          const hostId = localStorage.getItem('id');
-          const requestBody = JSON.stringify({
-            hostId,
-            gameMode,
-            questionAmount,
-            timer: timerValue,
-            gameFormat,
-          });
-          const response = await api.post('/game', requestBody);
-          const gameId = response.data.gameId;
-          history.push(`/game/${gameId}`);
-        } catch (error) {
-          console.error(`Something went wrong while creating the game: \n${handleError(error)}`);
-          console.error('Details:', error);
-          alert('Something went wrong while creating the game! See the console for details.');
-        }
-    };
-  
-    const handleClickFormat = (selectedOption) => {
-          console.log('Selected option:', selectedOption);
-          if (selectedOption === 'blitz') {
-              handleBlitz();
-          } else if (selectedOption === 'rapid') {
-              handleRapid();
-          } else if (selectedOption === 'custom') {
-              handleCustom();
-          }
-    };
 
- /*   const handleClickTheme = (selectedOption) => {
+    /*const handleClickTheme = (selectedOption) => {
             console.log('Selected option:', selectedOption);
             if (selectedOption === 'tv show') {
                 handleMode("SHOW", "but1");
@@ -104,18 +48,6 @@ const GameSelection = () => {
             };
     };*/
 
-    const handleClickTheme = (selectedOption2) => {
-        console.log('Selected option:', selectedOption2);
-        if (selectedOption2 === 'tv show') {
-            setGameMode("SHOW");
-        } else if (selectedOption2 === 'actors') {
-            setGameMode("ACTOR");
-        } else if (selectedOption2 === 'movies') {
-            setGameMode("MOVIE");
-        } else if (selectedOption2 === 'mixed') {
-            setGameMode("MIXED");
-        };
-};
     const createGame = async (gameMode) => {
         try {
             console.log(gameMode)
@@ -147,9 +79,6 @@ const GameSelection = () => {
         });*/
     };
 
-    function handleFormat(format) {
-        setGameFormat(format);
-    }
 
     const handleSliderChange = (event) => {
         setSliderValue(event.target.value);
@@ -163,13 +92,6 @@ const GameSelection = () => {
     return (
         <ThemeProvider theme={theme}>
             <Box className="box" sx={{ textAlign: 'center'}}>
-                    <h3>Game format:</h3>
-                    <Box className="row">
-                        <PrimaryButton label="custom" onClick={() => handleCustom()}/>
-                        <PrimaryButton label="blitz" onClick={() => handleBlitz()}/>
-                        <PrimaryButton label="rapid" onClick={() => handleRapid()}/>
-                        {/*<SplitButton options={optionsFormat} handleClick={handleClickFormat}/>*/}
-                    </Box>
                     <h3 class="center"> Game theme:</h3>
                     <Box className="row">
                         <PrimaryButton label="tv series" onClick={() => handleMode("SHOW", "but1")}/>
