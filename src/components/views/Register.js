b33f019013f362ef79ccc18531bb8068eee79ec8
florencehuegi <92146248+florencehuegi@users.noreply.github.com>
2023-05-21 15:26:17 +0200

    minor changes

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -10,7 +10,9 @@ import 'styles/mui/Box.scss';
 import 'styles/mui/Button.scss';
 import {Box, Button, FormControl, InputAdornment, InputLabel, IconButton, OutlinedInput } from '@mui/material';
 import {Visibility, VisibilityOff} from '@mui/icons-material'
-import { PrimaryButton, SecondaryButton } from 'styles/mui/PrimaryButton';
+import { PrimaryButton} from 'styles/mui/PrimaryButton';
+import PasswordInput from 'styles/mui/PasswordInput';
+import SecondaryButton from 'styles/mui/SecondaryButton';
 /*
 It is possible to add multiple components inside a single file,
 however be sure not to clutter your files with an endless amount!
@@ -73,7 +75,7 @@ const Register = props => {
     return (
         <ThemeProvider theme={theme}>
         <Box className="box">
-                <FormControl sx={{ m: 1, width: '60ch' }} variant="outlined">
+                <FormControl sx={{ m: 1, width: '90%' }} variant="outlined">
                     <InputLabel 
                     htmlFor="outlined-username"
                     sx={{ color: theme.palette.primary.light }}
@@ -101,81 +103,14 @@ const Register = props => {
                           }}
                     />
                 </FormControl>
-                <FormControl sx={{ m: 1, width: '60ch' }} variant="outlined">
-                    <InputLabel
-                        htmlFor="outlined-adornment-password"
-                        sx={{ color: theme.palette.primary.light }}
-                    >
-                        Password
-                    </InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-password"
-                        type={showPassword ? 'text' : 'password'}
-                        value={password}
-                        onChange={p => setPassword(p.target.value)}
-                        sx={{
-                            '& fieldset': {
-                              borderColor: theme.palette.primary.light,
-                            },
-                            '& input': {
-                              color: theme.palette.primary.light,
-                            },
-                          }}
-                        endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end"
-                            sx={{ color: theme.palette.primary.light }}
-                            >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                        }
-                        margin='dense'
-                        label="Password"
-                        onKeyPress={(event) => {
-                            if (event.key === " ") {
-                                event.preventDefault();
-                            }
-                        }}
-                    />
-                </FormControl>
+                <PasswordInput
+                    label="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
                   <Box className= "custom">      
-                    <Button 
-                        sx={{
-                            margin: 1,
-                            backgroundColor: theme.palette.primary.main,
-                            color: theme.palette.primary.text,
-                            opacity: !username || !password ? 0.5 : 1,
-                            "&:disabled": { 
-                                color: theme.palette.primary.text,
-                                backgroundColor: theme.palette.primary.main,
-                                opacity: 0.5
-                            }
-                        }}
-                        variant="contained"
-                        width="100%"
-                        disabled={!username || !password}
-                        onClick={() => doRegister()}
-                        >
-                        Register
-                    </Button>
-                    <Button 
-                        sx={{
-                            margin: 1,
-                            color: theme.palette.primary.main,
-                            borderColor: theme.palette.primary.main
-                        }}
-                        variant="outlined"
-                        width="100%"
-                        size='small'
-                        onClick={() => history.push("/login")}
-                    >
-                        Back
-                    </Button>
+                    <PrimaryButton label="register" onClick={() => doRegister()} disabled={!username || !password} />
+                    <SecondaryButton label="back" onClick={() => history.push("/login")} />
                 </Box>
             </Box>
           </ThemeProvider>
